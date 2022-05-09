5ebe1f95515d0491de6290ee3fbbe9607af31c14
Manav Misra <manavm@visionwebsoft.com>
2022-05-09 09:36:28 -0500

    "plugin:react/jsx-runtime"

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,18 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    'plugin:react/recommended',
-    'airbnb',
-    'prettier'
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:react/jsx-runtime",
+    "prettier",
   ],
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
     },
-    ecmaVersion: 'latest',
-    sourceType: 'module',
+    ecmaVersion: "latest",
+    sourceType: "module",
   },
-  plugins: [
-    'react',
-  ],
-  rules: {
-  },
+  plugins: ["react"],
+  rules: {},
 };
