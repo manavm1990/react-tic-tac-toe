2ea2f213167eff078b571b3490b08428812b8daf
Manav Misra <manavm@visionwebsoft.com>
2022-05-09 11:02:37 -0500

    Finish working TTT game

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import React from "react";
 import Header from "./components/Header";
 import Square from "./components/Square";
+import checkWinner from "./game.service";
 
 function App() {
-  const [board, setBoard] = React.useState(Array(9).fill("X"));
+  const [board, setBoard] = React.useState(Array(9).fill(""));
+  const [turn, setTurn] = React.useState("X");
+  const [winner, setWinner] = React.useState("");
+
+  const handleClick = (i) => {
+    if (winner) {
+      return;
+    }
+
+    const newBoard = [...board];
+
+    if (newBoard[i] === "") {
+      newBoard[i] = turn;
+      setBoard(newBoard);
+
+      if (checkWinner(newBoard, turn)) {
+        setWinner(turn);
+      } else {
+        setTurn((prev) => (prev === "X" ? "O" : "X"));
+      }
+    }
+  };
 
   return (
     <>
+      {/* TODO: Use the Winner state to announce the winner in the header. */}
       <Header />
       <main className="main">
         {board.map((mark, index) => (
-          <Square mark={mark} key={index} />
+          <Square
+            mark={mark}
+            key={index}
+            clickHandler={handleClick}
+            index={index}
+          />
         ))}
       </main>
     </>
--- src/components/Square.jsx
+++ src/components/Square.jsx
@@ -1,9 +1,25 @@
 import PropTypes from "prop-types";
 
-export default function Square({ mark }) {
-  return <div className="square">{mark}</div>;
+export default function Square({ index, mark, clickHandler }) {
+  return (
+    <div
+      className="square"
+      onClick={() => {
+        clickHandler(index);
+      }}
+      onKeyDown={() => {
+        clickHandler(index);
+      }}
+      role="button"
+      tabIndex={0}
+    >
+      {mark}
+    </div>
+  );
 }
 
 Square.propTypes = {
+  index: PropTypes.number.isRequired,
   mark: PropTypes.string.isRequired,
+  clickHandler: PropTypes.func.isRequired,
 };
